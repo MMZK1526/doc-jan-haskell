345575d6e2014d3e269ab1b665fdf0326266b732
mmzk1526 <yc4120@ic.ac.uk>
2023-05-20 13:49:10 +0100

    [mmzk] (feat) 2021 Part I to IV

--- src/Year2021/Alloc.hs
+++ src/Year2021/Alloc.hs
@@ -5,58 +5,69 @@ import Data.List
 
 import Year2021.Types
 import Year2021.Examples
+import Data.Ord
+import Data.Bifunctor
 
 ------------------------------------------------------
 --
 -- Part I
 --
 count :: Eq a => a -> [a] -> Int
-count 
-  = undefined
+count = (length .) . filter . (==)
 
 degrees :: Eq a => Graph a -> [(a, Int)]
-degrees 
-  = undefined
+degrees (ns, es) = [(n, count n $ concatMap (\(x, y) -> [x, y]) es) | n <- ns]
 
 neighbours :: Eq a => a -> Graph a -> [a]
-neighbours 
-  = undefined
+neighbours n (ns, es) = [y | (x, y) <- es, n == x] ++ [x | (x, y) <- es, n == y]
 
 removeNode :: Eq a => a -> Graph a -> Graph a
-removeNode 
-  = undefined
+removeNode n (ns, es) = (ns \\ [n], [(x, y) | (x, y) <- es, x /= n, y /= n])
 
 ------------------------------------------------------
 --
 -- Part II
 --
 colourGraph :: (Ord a, Show a) => Int -> Graph a -> Colouring a
-colourGraph 
-  = undefined
+colourGraph _ ([], _) = []
+colourGraph k g       = (n, c) : cMap'
+  where
+    (n, d) = minimumBy (comparing snd) (degrees g)
+    ns     = neighbours n g
+    g'     = removeNode n g
+    cMap'  = colourGraph k g'
+    ncs    = map (`lookUp` cMap') ns
+    c      = fromMaybe 0 $ listToMaybe [c | c <- [1..k], c `notElem` ncs]
 
 ------------------------------------------------------
 --
 -- Part III
 --
 buildIdMap :: Colouring Id -> IdMap
-buildIdMap 
-  = undefined
+buildIdMap = (("return", "return") :) . map (second (('R' :) . show))
 
 buildArgAssignments :: [Id] -> IdMap -> [Statement]
-buildArgAssignments 
-  = undefined
+buildArgAssignments xs idMap = map (\x -> Assign (lookUp x idMap) (Var x)) xs
 
 renameExp :: Exp -> IdMap -> Exp
 -- Pre: A precondition is that every variable referenced in 
 -- the expression is in the idMap. 
-renameExp 
-  = undefined
+renameExp (Const i) _   = Const i
+renameExp (Var x) idMap = Var (lookUp x idMap)
+renameExp (Apply op e1 e2) idMap
+  = Apply op (renameExp e1 idMap) (renameExp e2 idMap)
 
 renameBlock :: Block -> IdMap -> Block
 -- Pre: A precondition is that every variable referenced in 
 -- the block is in the idMap. 
-renameBlock 
-  = undefined
+renameBlock b idMap = map renameStatement b
+  where
+    renameStatement (Assign x e)
+      = Assign (lookUp x idMap) (renameExp e idMap)
+    renameStatement (If e b1 b2)
+      = If (renameExp e idMap) (renameBlock b1 idMap) (renameBlock b2 idMap)
+    renameStatement (While e b)
+      = While (renameExp e idMap) (renameBlock b idMap)
 
 renameFun :: Function -> IdMap -> Function
 renameFun (f, as, b) idMap
@@ -67,8 +78,10 @@ renameFun (f, as, b) idMap
 -- Part IV
 --
 buildIG :: [[Id]] -> IG
-buildIG 
-  = undefined
+buildIG liveVars = (ns, es)
+  where
+    ns = nub $ concat liveVars
+    es = nub $ concatMap (\xs -> [(x, y) | x <- xs, y <- xs, x < y]) liveVars
 
 -----------------------------------------------------
 --
