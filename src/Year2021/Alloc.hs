9e135cb3e8ed9996abdc9802c83a44b592c3688c
mmzk1526 <yc4120@ic.ac.uk>
2023-05-20 13:02:36 +0100

    [mmzk] (feat) 2021 skeleton

---
+++ src/Year2021/Alloc.hs
@@ -0,0 +1,83 @@
+module Year2021.Alloc where
+
+import Data.Maybe
+import Data.List
+
+import Year2021.Types
+import Year2021.Examples
+
+------------------------------------------------------
+--
+-- Part I
+--
+count :: Eq a => a -> [a] -> Int
+count 
+  = undefined
+
+degrees :: Eq a => Graph a -> [(a, Int)]
+degrees 
+  = undefined
+
+neighbours :: Eq a => a -> Graph a -> [a]
+neighbours 
+  = undefined
+
+removeNode :: Eq a => a -> Graph a -> Graph a
+removeNode 
+  = undefined
+
+------------------------------------------------------
+--
+-- Part II
+--
+colourGraph :: (Ord a, Show a) => Int -> Graph a -> Colouring a
+colourGraph 
+  = undefined
+
+------------------------------------------------------
+--
+-- Part III
+--
+buildIdMap :: Colouring Id -> IdMap
+buildIdMap 
+  = undefined
+
+buildArgAssignments :: [Id] -> IdMap -> [Statement]
+buildArgAssignments 
+  = undefined
+
+renameExp :: Exp -> IdMap -> Exp
+-- Pre: A precondition is that every variable referenced in 
+-- the expression is in the idMap. 
+renameExp 
+  = undefined
+
+renameBlock :: Block -> IdMap -> Block
+-- Pre: A precondition is that every variable referenced in 
+-- the block is in the idMap. 
+renameBlock 
+  = undefined
+
+renameFun :: Function -> IdMap -> Function
+renameFun (f, as, b) idMap
+  = (f, as, buildArgAssignments as idMap ++ renameBlock b idMap)
+
+-----------------------------------------------------
+--
+-- Part IV
+--
+buildIG :: [[Id]] -> IG
+buildIG 
+  = undefined
+
+-----------------------------------------------------
+--
+-- Part V
+--
+liveVars :: CFG -> [[Id]]
+liveVars 
+  = undefined
+
+buildCFG :: Function -> CFG
+buildCFG 
+  = undefined
